d1b941f0427e0db22652a7ebdb11fef1f26d6f57
Jacob O'Neill <jacoboneill2000@outlook.com>
2026-03-11 14:28:15 +0000

    Add handler and decompose main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
+	db "github.com/jacoboneill/securenote/internal/db"
+	"github.com/jacoboneill/securenote/internal/handlers"
 	_ "modernc.org/sqlite"
-	// "github.com/jacoboneill/securenote/internal/db"
 )
 
 func logging(next http.Handler) http.Handler {
@@ -20,16 +20,40 @@ func logging(next http.Handler) http.Handler {
 	})
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	id := r.URL.Query().Get("id")
-	if _, err := fmt.Fprintf(w, "Hello, world! %s", id); err != nil {
-		log.Println(err)
+func initDB(dataSourceName string) (*sql.DB, error) {
+	conn, err := sql.Open("sqlite", dataSourceName)
+	return conn, err
+}
+
+func runMigrations(conn *sql.DB, sourceURL string) error {
+	driver, err := sqlite.WithInstance(conn, &sqlite.Config{})
+	if err != nil {
+		return err
 	}
+
+	m, err := migrate.NewWithDatabaseInstance(sourceURL, "sqlite", driver)
+	if err != nil {
+		return err
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return err
+	}
+
+	return nil
+}
+
+func initMux(queries *db.Queries) http.Handler {
+	h := handlers.New(queries)
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /users", h.CreateUser)
+
+	return logging(mux)
 }
 
 func main() {
-	// Get Database connection
-	conn, err := sql.Open("sqlite", "securenote.db")
+	conn, err := initDB("securenote.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,29 +63,16 @@ func main() {
 		}
 	}()
 
-	// Run Migrations
-	driver, err := sqlite.WithInstance(conn, &sqlite.Config{})
-	if err != nil {
+	queries := db.New(conn)
+
+	if err := runMigrations(conn, "file://migrations"); err != nil {
 		log.Fatal(err)
 	}
 
-	m, err := migrate.NewWithDatabaseInstance("file://migrations", "sqlite", driver)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
-		log.Fatal(err)
-	}
-
-	log.Println("migrations applied")
-
-	// Setup Handlers
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+	mux := initMux(queries)
 
 	// Start Server
 	const addr = ":8080"
 	log.Printf("listening on %s", addr)
-	log.Fatal(http.ListenAndServe(addr, logging(mux)))
+	log.Fatal(http.ListenAndServe(addr, mux))
 }
